7d350b86df2e9c07a6dee4c1b79f1f131e08acae
lixu <291332445@qq.com>
2021-01-26 21:43:34 +0800

    0126 21:43

---
+++ src/components/common/tabbar/TabBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="tab-bar">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+/**
+@description:
+@createTime:2021-01-18
+@create:改成你自己的名字
+*/
+export default {
+  name: "TabBar",
+  props: {},
+  components: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  mounted() {},
+  created() {},
+  methods: {},
+};
+</script>
+<style scoped>
+#tab-bar {
+  display: flex;
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #f6f6f6;
+  box-shadow: 0 -1px 1px rgba(0, 0, 0, 0.1);
+}
+</style>
